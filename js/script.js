f19deda19eb3aafd7f06b3ce07e355cbd6dc3cb9
OrcRaccoon <karina.udaltsova@gmail.com>
2017-05-05 14:25:41 +0300

    js отдельным файлом

---
+++ js/script.js
@@ -0,0 +1,12 @@
+            var link = document.querySelector(".feedback-form-btn");
+            var popup = document.querySelector(".feedback-modal-wrapper");
+            var close = popup.querySelector(".close");
+
+            link.addEventListener("click", function(event) {
+                event.preventDefault();
+                popup.classList.add("feedback-modal-wrapper-show");
+            });
+            close.addEventListener("click", function(event) {
+                event.preventDefault();
+                popup.classList.remove("feedback-modal-wrapper-show");
+            });
